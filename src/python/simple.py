3d29438147a7f8d9668d8fe42915377322204d03
Pablo <pablo.aledo@gmail.com>
2020-11-17 03:15:41 +0000

    refactor, add some structure

--- src/python/simple.py
+++ src/python/simple.py
@@ -3,49 +3,76 @@ import os
 import spacy
 import zipfile
 from pymongo import MongoClient
+from flask import Flask
 
-with zipfile.ZipFile(os.path.join('data', 'uspat1_201831_back_80001_100000.zip'), 'r') as zip_ref:
-    zip_ref.extractall(os.path.join('data', 'uspat1_201831_back_80001_100000'))
 
-with open(os.path.join('data', 'uspat1_201831_back_80001_100000', 'US06179885B2.xml')) as file:
-    data = file.read().replace('<br/>','')
+def get_description(data):
+    root = ET.fromstring(data)
+    element = root; element
+    element = element.find('description'); element
+    element = element.findall('p'); element
+    element = [ x.text for x in element ]; element
+    element = [ x.split('\n') for x in element ]; element
+    element = list(itertools.chain(*element)); element
+    element = [ x.strip() for x in element ]; element
+    element = [ x for x in element if 'isobutanol' in x ]; element
+    #element = [ element[10] ]; element
+    return element
 
-root = ET.fromstring(data)
-element = root; element
-element = element.find('description'); element
-element = element.findall('p'); element
-element = [ x.text for x in element ]; element
-element = [ x.split('\n') for x in element ]; element
-element = list(itertools.chain(*element)); element
-element = [ x.strip() for x in element ]; element
-element = [ x for x in element if 'isobutanol' in x ]; element
-#element = [ element[10] ]; element
+def get_abstract(data):
+    root = ET.fromstring(data)
+    element = root; element
+    element = element.find('abstract'); element
+    element = element.getchildren(); element
+    element = element[0]; element
+    element = element.text; element
+    element = element.split('\n'); element
+    element = filter( lambda x : x.strip() != '', element ); element
+    element = [ x.strip() for x in element ]; element
+    return element
 
-nlp = spacy.load(os.path.join('models', 'chemner'))
+def persist_data(application, year, title, abstract, fulltext):
+    client = MongoClient()
 
-[ nlp(unicode(x)).ents for x in element ]
+    db = client['patents']
+    collection = db['patents']
 
-root = ET.fromstring(data)
-element = root; element
-element = element.find('abstract'); element
-element = element.getchildren(); element
-element = element[0]; element
-element = element.text; element
-element = element.split('\n'); element
-element = filter( lambda x : x.strip() != '', element ); element
-element = [ x.strip() for x in element ]; element
+    entry = {
+          "application": application,
+          "year": year,
+          "title": title,
+          "abstract": abstract,
+          "fulltext": fulltext
+          }
 
-client = MongoClient()
+    db.collection.insert_one(entry)
 
-db = client['namedEntityRecognition']
-collection = db['patents']
+def loadArchive( zipName )
+    with zipfile.ZipFile(os.path.join('data', 'uspat1_201831_back_80001_100000.zip'), 'r') as zip_ref:
+        zip_ref.extractall(os.path.join('tmp', 'uspat1_201831_back_80001_100000'))
 
-entry1 = {
-      "application": "06176565",
-      "year": "2001",
-      "title": "Cleaning member for ink jet head and ink jet apparatus provided with said cleaning member",
-      "abstract": ["paragraph-1", "paragraph-2" ],
-      "fulltext": ["paragraph-1", "paragraph-2" ]
-      }
+    with open(os.path.join('tmp', 'uspat1_201831_back_80001_100000', 'US06179885B2.xml')) as file:
+        data = file.read().replace('<br/>','')
 
-db.collection.insert_one(entry1)
+    application = get_application(data)
+    year = get_year(data)
+    title = get_title(data)
+    description = get_description(data)
+    abstract = get_abstract(data)
+    fulltext = get_fulltext(data)
+
+    persist_data(application, year, title, description, abstract, fulltext)
+
+    nlp = spacy.load(os.path.join('models', 'chemner'))
+
+    [ nlp(unicode(x)).ents for x in element ]
+
+app = Flask(__name__)
+
+@app.route('/load/<zipId>')
+def show_user_profile(zipId):
+    loadArchive(zipId)
+    return 'zip loaded %s' % escape(zipId)
+
+if __name__ == '__main__':
+    app.run
